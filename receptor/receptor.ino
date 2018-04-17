7bac420a9321d332ed43afcc0fe48683e3d480a2
Unknown <scheunemanngame@gmail.com>
2018-04-17 10:47:34 -0300

    upload arquivos que foram utilizados no sistema para o teste estático

---
+++ receptor/receptor.ino
@@ -0,0 +1,37 @@
+#include <VirtualWire.h> //Insere a biblioteca VirtualWire
+#define R_IGNITOR 8          //Define o pino 8 para o IGNITOR
+char R_C_IGN[2];         //Armazena o estado do IGNITOR
+int R_BOT = 0;           //Configura o botao como nao acionado
+ 
+void setup() 
+{         
+  pinMode(R_IGNITOR, OUTPUT); //Configura o pino 8 como saída
+  vw_set_rx_pin(4);       //Configura o pino 4 como receptor
+  vw_setup(2000);         //Velocidade de trasmissao em bps
+  vw_rx_start();          //Inicializa o receptor
+}
+ 
+void loop()
+{                           
+    uint8_t buf[VW_MAX_MESSAGE_LEN];     //Armazena os dados
+    uint8_t buflen = VW_MAX_MESSAGE_LEN; //Define o tamanho dos dados
+    if(vw_get_message(buf, &buflen)) 
+    {   //Verifica se exite dados
+      int a;                             
+      for(a = 0; a < buflen; a++)
+      {        
+        R_C_IGN[a] = char(buf[a]);       //Armazena dados
+      }
+      R_C_IGN[buflen] = '\0';
+      int R_BOT = atoi(R_C_IGN);          //Converte os dados de char para int
+      if(R_BOT == 1)
+      {                    //Se o botao for acionado, aciona o IGNITOR
+        digitalWrite(R_IGNITOR, HIGH);        //Configura o IGNITOR com nivel logico alto             
+      }
+      if(R_BOT == 0) 
+      {                    //Se o botao nao for acionado, nao aciona o ignitor
+        digitalWrite(R_IGNITOR, LOW);         //Configura o ignitor como desligado
+    
+      }
+    }
+}
---
+++ transmissor/transmissor.ino
@@ -0,0 +1,38 @@
+#include <VirtualWire.h> //Insere a biblioteca VirtualWire
+#define T_BOT 5          //Define o pino 5 para o botao
+char T_C_IGNITOR[2];         //Armazena o estado do ignitor
+int T_IGNITOR = 0;           //Define o ignitor como nivel logico baixo
+int IGN_STATE= -1;
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
+  
+void setup() 
+{                 //Chama a função setup
+  pinMode(T_BOT,INPUT_PULLUP); //Configura o pino 5 como entrada
+  vw_set_tx_pin(2);            //Configura o pino 2 como transmissor
+  vw_setup(2000);              //Velocidade de trasmissao em bps
+}
+ 
+void loop()
+{
+    if ( (millis() - lastDebounceTime) > debounceDelay)
+    {
+      if((digitalRead(T_BOT) == LOW) && (IGN_STATE <0))
+      {                 //Configura o botao como acionado
+        T_IGNITOR = !T_IGNITOR;                               //Inverte o estado do ignitor
+        itoa(T_IGNITOR, T_C_IGNITOR, 2);                      //Converte os dados de int para char
+        vw_send((uint8_t *)T_C_IGNITOR, strlen(T_C_IGNITOR)); //Envia os dados
+        vw_wait_tx();                                 //Aguarda a transmissao de dados
+        lastDebounceTime = millis(); //set the current time
+        IGN_STATE=-IGN_STATE;
+        delay(100);                                   //Tempo em que o ignitor permanece em alto
+    
+     }
+     else if ((digitalRead(T_BOT)==HIGH) && (IGN_STATE>0))
+     {
+       lastDebounceTime = millis(); //set the current time
+       IGN_STATE=-IGN_STATE;
+     }
+    }
+}
+
